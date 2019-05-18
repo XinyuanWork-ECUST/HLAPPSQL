456ad2b8dc8431e56a357bf6b762f20dabb1146b
xuxiaojin <1319039722@qq.com>
2019-05-18 15:56:28 +0800

    commit by xujin

--- src/main/java/com/xxx/demo/Controller/StuCPCController.java
+++ src/main/java/com/xxx/demo/Controller/StuCPCController.java
@@ -20,21 +20,21 @@ public class StuCPCController {
     @Autowired
     StuCPCService stucpcService;
 
-    @PostMapping("/api/stucpc/createStuCPC")
-    public Response createstuCPC(@RequestParam String stuID,@RequestParam boolean isCCYL,@RequestParam String CCYLJoinDate,@RequestParam boolean requisitionSubmitted,@RequestParam String requisitionSubmitDate,@RequestParam boolean intermediatePartySchool, @RequestParam String intermediatePartySchoolDate,@RequestParam String intermediatePartySchoolLead,@RequestParam double intermediatePartySchoolScore,@RequestParam boolean seniorPartySchool,@RequestParam String seniorPartySchoolRequisitionSubmitDate,@RequestParam String seniorPartySchoolDate,@RequestParam double seniorPartySchoolScore, @RequestParam int seniorPartySchoolNo,@RequestParam boolean haveJoin,@RequestParam String joinDate,@RequestParam String condition) {
-        try{
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date ccylJoinDate=format.parse(CCYLJoinDate);
-            Date requisitionsubmitDate=format.parse(requisitionSubmitDate);
-            Date intermediatepartySchoolDate=format.parse(intermediatePartySchoolDate);
-            Date seniorpartyschoolDate=format.parse(seniorPartySchoolDate);
-            Date seniorpartyschoolrequisitionSubmitDate=format.parse(seniorPartySchoolRequisitionSubmitDate);
-            Date joindate=format.parse(joinDate);
-            stucpcService.createStuCPC(stuID,isCCYL,ccylJoinDate,requisitionSubmitted,requisitionsubmitDate,intermediatePartySchool,intermediatepartySchoolDate,intermediatePartySchoolLead,intermediatePartySchoolScore,seniorPartySchool,seniorpartyschoolrequisitionSubmitDate,seniorpartyschoolDate,seniorPartySchoolScore,seniorPartySchoolNo,haveJoin,joindate,condition);
-            return genSuccessResult(true);
-        }catch (Exception e){
-            return genFailResult("添加失败");
-        }
-    }
+//    @PostMapping("/api/stucpc/createStuCPC")
+//    public Response createstuCPC(@RequestParam String stuID,@RequestParam boolean isCCYL,@RequestParam String CCYLJoinDate,@RequestParam boolean requisitionSubmitted,@RequestParam String requisitionSubmitDate,@RequestParam boolean intermediatePartySchool, @RequestParam String intermediatePartySchoolDate,@RequestParam String intermediatePartySchoolLead,@RequestParam double intermediatePartySchoolScore,@RequestParam boolean seniorPartySchool,@RequestParam String seniorPartySchoolRequisitionSubmitDate,@RequestParam String seniorPartySchoolDate,@RequestParam double seniorPartySchoolScore, @RequestParam int seniorPartySchoolNo,@RequestParam boolean haveJoin,@RequestParam String joinDate,@RequestParam String condition) {
+//        try{
+//            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            Date ccylJoinDate=format.parse(CCYLJoinDate);
+//            Date requisitionsubmitDate=format.parse(requisitionSubmitDate);
+//            Date intermediatepartySchoolDate=format.parse(intermediatePartySchoolDate);
+//            Date seniorpartyschoolDate=format.parse(seniorPartySchoolDate);
+//            Date seniorpartyschoolrequisitionSubmitDate=format.parse(seniorPartySchoolRequisitionSubmitDate);
+//            Date joindate=format.parse(joinDate);
+//            //stucpcService.createStuCPC(stuID,isCCYL,ccylJoinDate,requisitionSubmitted,requisitionsubmitDate,intermediatePartySchool,intermediatepartySchoolDate,intermediatePartySchoolLead,intermediatePartySchoolScore,seniorPartySchool,seniorpartyschoolrequisitionSubmitDate,seniorpartyschoolDate,seniorPartySchoolScore,seniorPartySchoolNo,haveJoin,joindate,condition);
+//            return genSuccessResult(true);
+//        }catch (Exception e){
+//            return genFailResult("添加失败");
+//        }
+//    }
 
 }
