9d1a17aace0f4b54f85f38d67aa7830cec1ef3c6
xuxiaojin <1319039722@qq.com>
2019-05-18 16:45:42 +0800

    Revert "commit by xujin"
    This reverts commit 456ad2b8dc8431e56a357bf6b762f20dabb1146b.

--- src/main/java/com/xxx/demo/Service/StuCPCService.java
+++ src/main/java/com/xxx/demo/Service/StuCPCService.java
@@ -11,12 +11,12 @@ public class StuCPCService {
     @Autowired
     StuCPCRepository stucpcRepository;
 
-//    public boolean createStuCPC(String stuID, boolean isCCYL, Date CCYLJoinDate, boolean requisitionSubmitted, Date requisitionSubmitDate, boolean intermediatePartySchool, Date intermediatePartySchoolDate, String intermediatePartySchoolLead, double intermediatePartySchoolScore, boolean seniorPartySchool, Date seniorPartySchoolRequisitionSubmitDate, Date seniorPartySchoolDate, double seniorPartySchoolScore, int seniorPartySchoolNo, boolean haveJoin, Date joinDate, String condition){
-//        try{
-//            stucpcRepository.createStuCPC(stuID,isCCYL,CCYLJoinDate,requisitionSubmitted,requisitionSubmitDate,intermediatePartySchool,intermediatePartySchoolDate,intermediatePartySchoolLead,intermediatePartySchoolScore,seniorPartySchool,seniorPartySchoolRequisitionSubmitDate,seniorPartySchoolDate,seniorPartySchoolScore,seniorPartySchoolNo,haveJoin,joinDate,condition);
-//            return true;
-//        }catch (Exception e){
-//            return false;
-//        }
-//    }
+    public boolean createStuCPC(String stuID, boolean isCCYL, Date CCYLJoinDate, boolean requisitionSubmitted, Date requisitionSubmitDate, boolean intermediatePartySchool, Date intermediatePartySchoolDate, String intermediatePartySchoolLead, double intermediatePartySchoolScore, boolean seniorPartySchool, Date seniorPartySchoolRequisitionSubmitDate, Date seniorPartySchoolDate, double seniorPartySchoolScore, int seniorPartySchoolNo, boolean haveJoin, Date joinDate, String condition){
+        try{
+            stucpcRepository.createStuCPC(stuID,isCCYL,CCYLJoinDate,requisitionSubmitted,requisitionSubmitDate,intermediatePartySchool,intermediatePartySchoolDate,intermediatePartySchoolLead,intermediatePartySchoolScore,seniorPartySchool,seniorPartySchoolRequisitionSubmitDate,seniorPartySchoolDate,seniorPartySchoolScore,seniorPartySchoolNo,haveJoin,joinDate,condition);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
